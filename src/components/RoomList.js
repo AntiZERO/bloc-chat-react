1f329fce19d76ab6c27ec4bc821753e554eebf6b
Gordon Ray <gray2884@gmail.com>
2019-01-31 00:20:11 -0600

    assignment-2765 completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
 
 class RoomList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { rooms: [] };	
-		this.roomsRef = this.props.firebase.database().ref('rooms');
-   	}
-   	componentDidMount() {
-   		this.roomsRef.on('child_added', snapshot => {
-   			const room = snapshot.val();
-   			room.key = snapshot.key;
-   			this.setState({ rooms: this.state.rooms.concat( room ) });
-   		});
-   	}
-   	render() { 
-   		const roomList = this.state.rooms.map((room ) => <li key={room.key}>{room.name}</li>); 
-   		return (<ul>{roomList}</ul>);
-   	}
+    constructor(props) {
+        super(props);
+        this.state = { 
+            rooms: [],
+            value: '' 
+        }; 
+
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat( room ) });
+        });
+    }
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        this.roomsRef.push({
+            name: this.state.value
+        });
+
+    }
+
+    render() { 
+        
+        const roomList = this.state.rooms.map((room) => 
+            <li key={room.key}>{room.name}</li>
+        ); 
+
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Create a Chat Room: 
+                    <input type='text' value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type='submit' value='Submit' />
+            </form>
+            <ul>{roomList}</ul>
+            </div>
+
+        );
+    }
 }
 export default RoomList;
-
 
 
